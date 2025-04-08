bffb39ea60a24850ecdeaad5f6a0e880e01418d2
Matheus Souza <matheusouza1997@gmail.com>
2025-04-08 11:46:49 -0300

    Inclusção das opções de PWA

---
+++ public/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"2df3d4bc9c1184524aec426a2ef92a25","url":"index.html"},{"revision":"ff5da0b292612186186be55009060875","url":"service-worker.js"}]);
