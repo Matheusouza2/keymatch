41335001fecf87972babfaa585315443abba6996
Matheus Souza <matheusouza1997@gmail.com>
2025-04-08 11:52:17 -0300

    Inclusção das opções de PWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="manifest" href="public/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
 
             <script>
-                navigator.serviceWorker.register("public/pwabuilder-adv-sw.js")
+                navigator.serviceWorker.register("/pwabuilder-adv-sw.js")
             </script>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
