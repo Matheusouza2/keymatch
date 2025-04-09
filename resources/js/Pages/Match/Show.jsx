aba8b4227ef8bbb0df4644c5a2324b4176f0d3cc
Matheus Souza <matheusouza1997@gmail.com>
2025-04-09 15:50:06 -0300

    Amostragem do time mesmo que não seja o dono

--- resources/js/Pages/Match/Show.jsx
+++ resources/js/Pages/Match/Show.jsx
@@ -38,23 +38,23 @@ export default function Show({ match, title, teamAuth, tests }) {
 
             <div className="flex justify-between items-center mt-24 mx-4">
                 <div className="flex flex-col items-center">
-                    <img src={match.team1.image} className="size-28 rounded-full" alt="" />
-                    <h2 className="text-lg font-semibold">{match.team1.name}</h2>
-                    <h5 className="text-4xl font-semibold">{placares.length == 0 ? match.team1_score : placares.team1}</h5>
+                    <img src={match?.team1?.image} className="size-28 rounded-full" alt="" />
+                    <h2 className="text-lg font-semibold">{match?.team1?.name}</h2>
+                    <h5 className="text-4xl font-semibold">{placares?.length == 0 ? match?.team1_score : placares?.team1}</h5>
                 </div>
 
                 <span className="text-6xl font-bold">X</span>
 
                 <div className="flex flex-col items-center">
-                    <img src={match.team2.image} className="size-28 rounded-full" alt="" />
-                    <h2 className="text-lg font-semibold">{match.team2.name}</h2>
-                    <h5 className="text-4xl font-semibold">{placares.length == 0 ? match.team2_score : placares.team2}</h5>
+                    <img src={match?.team2?.image} className="size-28 rounded-full" alt="" />
+                    <h2 className="text-lg font-semibold">{match?.team2.name}</h2>
+                    <h5 className="text-4xl font-semibold">{placares?.length == 0 ? match?.team2_score : placares?.team2}</h5>
                 </div>
             </div>
 
             <div className="flex justify-end mx-5 mt-10">
                 {
-                    teamAuth.id == match.team1_id ?
+                    teamAuth?.id == match?.team1_id ?
                         (<div>
                             {
                                 tests.map((test, index) => {
